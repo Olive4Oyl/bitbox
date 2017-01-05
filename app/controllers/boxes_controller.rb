1e7b2853cf847fcac6e066b8d49b1782770e729a
Kelene Chung <simoy93@gmail.com>
2017-01-05 15:54:43 -0500

    render json

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -2,6 +2,10 @@ class BoxesController < ApplicationController
 
 	def index
 		@boxes = Box.all
+		respond_to do |format|
+			format.html { render :index }
+			format.json { render json: @boxes.to_json}
+		end
 	end
 
 end
--- app/models/box.rb
+++ app/models/box.rb
@@ -1,4 +1,9 @@
 class Box < ApplicationRecord
 	has_many :subscriptions
 	has_many :users, through: :subscriptions
+
+	def print_price
+		'%.2f' %(self.price/100.0)
+	end
+
 end
