98211b8211fec94f5507012f454e29e05ede9cba
Kelene Chung <simoy93@gmail.com>
2017-01-07 13:03:07 -0500

    update the subscription schema and seed info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105182729) do
+ActiveRecord::Schema.define(version: 20170106215046) do
 
   create_table "boxes", force: :cascade do |t|
     t.string   "title"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170105182729) do
     t.string   "description"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "subtitle"
+    t.string   "shipping_info"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,7 @@
 Box.create(title: "Rule Your Space Box", price: 9995, description: "Build touch-activated inventions to control your stuff. Great for kids & younger makers.", age_recommendation: 8, image: "https://s3.amazonaws.com/lb-spree/spree/products/3046/large/ryr-kit.jpg?1477951666")
 Box.create(title: "Arduino Coding Box", price: 9995, description: "Create programmable inventions while learn basic coding. Great for older kids & prototypers.", age_recommendation: 12, image: "https://s3.amazonaws.com/lb-spree/spree/products/3033/large/ack-kit.jpg?1471356817")
 Box.create(title: "Smart House Box", price: 24900, description: "Create DIY smart home inventions that you control over the internet. Great for DIY’ers.", age_recommendation: 16, image: "https://s3.amazonaws.com/lb-spree/spree/products/3006/large/SHK-Reskin-wBits_HR.jpg?1469125020")
+
+Subscription.create(level: "$10/month", subtitle: "Have an little inventor?", description: "Inspire their creativity with the right tools in this month's bite-size BitBox (7 BITS & 3 Accessories).", shipping_info: "Free Shipping*" )
+Subscription.create(level: "$30/month", subtitle: "Have an little inventor?", description: "Inspire their creativity with the right tools in this month's medium-size BitBox (8 BITS & 18 Accessories).", shipping_info: "Free Shipping*" )
+Subscription.create(level: "$100/month", subtitle: "Have an little inventor?", description: "Inspire their creativity with the right tools in this month's large-size BitBox (304 BITS & 228 Accessories).", shipping_info: "Free Shipping*" )
