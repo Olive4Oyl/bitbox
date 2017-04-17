0cb22d9635c0758914945e857609acf320c60e21
Kelene Chung <simoy93@gmail.com>
2017-04-16 23:20:58 -0400

    update styling for user's show, edit, subscribe

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,10 +6,17 @@ function getUsers(){
 			dataType: "json",
 		}).done(function(response){
 			var string = "<h2>" + response.name +"'s Subscription </h2>"
-			$('.user-container').prepend(string)
+			$('.card-info').prepend(string)
 		});
 
 	}
 }
 
+function userButton(){
+$(".btn-pref .btn").click(function () {
+    $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+    // $(".tab").addClass("active"); // instead of this do the below 
+    $(this).removeClass("btn-default").addClass("btn-primary");   
+});
+};
 
